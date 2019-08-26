028b8fba27db60bb310144e25d4d2cd3126a89a0
lucassousa-r <54552793+lucassousa-r@users.noreply.github.com>
2019-08-26 16:08:00 -0300

    Add files via upload
    This is a program that calculates areas of regular polygons

---
+++ area.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 10 12:39:08 2019
+
+@author: lucassr
+"""
+
+lotea = float(input('qual eh a largura?'))
+loteb = float(input('qual eh o comprimento?'))
+area = float(lotea*loteb)
+
+
+print (f'A área do seu lote é {area} m2')
+
