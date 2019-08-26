4aa0cf6328564d20ab353e7c6ab87b6a2794861b
lucassousa-r <54552793+lucassousa-r@users.noreply.github.com>
2019-08-26 16:15:39 -0300

    Add files via upload

---
+++ elevado.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 10 14:36:31 2019
+
+@author: lucassr
+"""
+
+numa = float(input('digite o numero da base'))
+numb = float(input('digite o numero para ser elevado'))
+ele = float(numa**numb)
+
+print (f'O resultado de {numa} elevado a {numb} eh {ele}')
